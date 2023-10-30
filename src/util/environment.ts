7c37f7e2b06f85a65aae189e0e5d4059627b73c3
Dan Goje <gojedan98@gmail.com>
2023-10-30 22:28:16 +0200

    improve readibility for util/environment

--- src/util/environment.ts
+++ src/util/environment.ts
@@ -3,35 +3,31 @@ export { asString, asNumber };
 function asString(variableName: string, defaultValue?: string): string {
     const variable = process.env[variableName];
 
-    if (variable !== undefined) {
+    if (variable) {
         return String(variable);
     }
-
-    if (defaultValue === undefined) {
-        throw Error(`Missing environment variable ${variableName}`);
+    if (defaultValue) {
+        console.warn(`Using default value ${defaultValue} for variable ${variableName}`);
+        return defaultValue;
     }
 
-    console.warn(`Using default value ${defaultValue} for variable ${variableName}`);
-
-    return defaultValue;
+    throw Error(`Missing environment variable ${variableName}`);
 }
 
 function asNumber(variableName: string, defaultValue?: number): number {
     const variable = process.env[variableName];
 
-    if (variable !== undefined) {
+    if (variable) {
         const numVar = Number(variable);
         if (Number.isNaN(numVar)) {
             throw Error(`Invalid value ${variable} for variable ${variableName}`);
         }
         return numVar;
     }
-
-    if (defaultValue === undefined) {
-        throw Error(`Missing environment variable ${variableName}`);
+    if (defaultValue) {
+        console.warn(`Using default value ${defaultValue} for variable ${variableName}`);
+        return defaultValue;
     }
 
-    console.warn(`Using default value ${defaultValue} for variable ${variableName}`);
-
-    return defaultValue;
+    throw Error(`Missing environment variable ${variableName}`);
 }
